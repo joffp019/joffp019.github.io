a164db883339212363bbccef9f53c57b33d08618
joffp019 <pjoffily@gmail.com>
2015-10-23 19:40:00 +0000

    msg ...

---
+++ code.js
@@ -0,0 +1,14 @@
+var array = ["zero", "one", "two", "three", "four" ];
+
+var numbers = [0, 1, 2, 3, 4];
+
+for (var index = 0; index < 5; index++) {
+	console.log(array[index]);
+	var mult = 555;
+	console.log(numbers[index]*mult);
+	var sign = 14641;
+		for (var i = 0; i < 4; i++) {
+			sign = sign/11;
+			console.log(sign);	
+	}
+}
